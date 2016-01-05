1ba681760d7116eac5b3cb54ed433e8e6092915c
Ross Mooney <ross@boomtownroi.com>
2016-01-05 10:24:21 -0500

    Created easy functions for pushing view controllers onto the navigation controller stack

---
+++ NavigationManager/NavigationManager.swift
@@ -0,0 +1,70 @@
+//
+//  NavigationManager.swift
+//  NavigationManager
+//
+//  Created by Ross M Mooney on 1/5/16.
+//  Copyright © 2016 Ross Mooney. All rights reserved.
+//
+
+import UIKit
+
+
+
+class NavigationManager {
+    static let singleton = NavigationManager()
+    
+    var navigationController:UINavigationController!
+    var currentPresentation:Presentation = .Root
+    
+    enum ViewControllerType : String {
+        case Red = "RedViewController"
+        case Blue = "BlueViewController"
+        case Black = "BlackViewController"
+        case White = "WhiteViewController"
+    }
+    
+    enum Presentation {
+        case Root
+        case Replace
+        case Push
+        case Modal(completion: (() -> Void)?)
+    }
+    
+    init() {
+        
+    }
+    
+    func setupWithNavigationController(navigationController: UINavigationController?) {
+        if let nav = navigationController where self.navigationController == nil {
+            self.navigationController = nav
+        }
+    }
+    
+    func pushVC(vc: ViewControllerType, presentation:Presentation = .Push) {
+        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier(vc.rawValue)
+
+        if case .Modal(_) = self.currentPresentation {
+            //Clear modal if we are presenting one
+            self.navigationController.dismissViewControllerAnimated(true, completion: { self.presentVC(viewController, presentation: presentation) })
+        } else {
+            self.presentVC(viewController, presentation: presentation)
+        }
+    }
+    
+    private func presentVC(viewController: UIViewController, presentation: Presentation) {
+        switch presentation {
+        case .Push:
+            self.navigationController.pushViewController(viewController, animated: true)
+        case .Root:
+            self.navigationController.setViewControllers([viewController], animated: true)
+        case .Modal(let completion):
+            self.navigationController.presentViewController(viewController, animated: true, completion: completion)
+        case .Replace:
+            var viewControllers = Array(self.navigationController.viewControllers.dropLast())
+            viewControllers.append(viewController)
+            self.navigationController.setViewControllers(viewControllers, animated: true)
+        }
+        self.currentPresentation = presentation
+    }
+    
+}
---
+++ NavigationManager/ViewControllers.swift
@@ -0,0 +1,135 @@
+//
+//  ViewController.swift
+//  NavigationManager
+//
+//  Created by Ross M Mooney on 1/5/16.
+//  Copyright © 2016 Ross Mooney. All rights reserved.
+//
+
+import UIKit
+
+protocol Pusher { }
+extension Pusher where Self : UIViewController {
+    func showAnotherViewController(type: NavigationManager.ViewControllerType) {
+        NavigationManager.singleton.pushVC(type)
+    }
+    
+    func setNewRootViewController(type: NavigationManager.ViewControllerType) {
+        NavigationManager.singleton.pushVC(type, presentation: .Root)
+    }
+    
+    func replaceViewController(type: NavigationManager.ViewControllerType) {
+        NavigationManager.singleton.pushVC(type, presentation: .Replace)
+    }
+    
+    func showModalViewController(type: NavigationManager.ViewControllerType, completionHandler: (() -> Void)?) {
+        NavigationManager.singleton.pushVC(type, presentation: .Modal(completion: completionHandler))
+    }
+}
+
+
+class RedViewController: UIViewController, Pusher {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //This is the main view controller so we can save the nav controller from here
+        NavigationManager.singleton.setupWithNavigationController(self.navigationController)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showBlue() {
+        setNewRootViewController(.Blue)
+    }
+    
+    @IBAction func showBlack() {
+        setNewRootViewController(.Black)
+    }
+    
+    @IBAction func showWhite() {
+        setNewRootViewController(.White)
+    }
+}
+
+class BlueViewController: UIViewController, Pusher {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showRed() {
+        showModalViewController(.Red, completionHandler: { print("Showing Red") })
+    }
+    
+    @IBAction func showBlack() {
+        showModalViewController(.Black, completionHandler: { print("Showing Black") })
+    }
+    
+    @IBAction func showWhite() {
+        showModalViewController(.White, completionHandler: { print("Showing White") })
+    }
+}
+
+
+class BlackViewController: UIViewController, Pusher {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showBlue() {
+        replaceViewController(.Blue)
+    }
+    
+    @IBAction func showRed() {
+        replaceViewController(.Red)
+    }
+    
+    @IBAction func showWhite() {
+        replaceViewController(.White)
+    }
+}
+
+
+class WhiteViewController: UIViewController, Pusher {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showBlue() {
+        showAnotherViewController(.Blue)
+    }
+    
+    @IBAction func showBlack() {
+        showAnotherViewController(.Black)
+    }
+    
+    @IBAction func showRed() {
+        showAnotherViewController(.Red)
+    }
+}
+
